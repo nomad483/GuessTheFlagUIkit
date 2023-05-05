2158b6682143fa567c469725497e8f38210defe9
nomad483 <nicolaizakluka@gmail.com>
2023-05-06 01:03:26 +0300

    first commit

--- Project2UIkit/ViewController.swift
+++ Project2UIkit/ViewController.swift
@@ -8,12 +8,71 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    var askNumber = 0
+    
+    let maxCountAsk = 10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "spain", "ukraine", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
     }
-
-
+    
+    func askQuestion(action: UIAlertAction! = nil) -> Void {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        title = "Score: \(score). Tap on \(countries[correctAnswer].uppercased())'s flag"
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct."
+            score += 1
+        } else {
+            title = "Wrong! That’s the flag of \(countries[sender.tag].uppercased())"
+            score -= 1
+        }
+        
+        var message = "Your score is \(score)."
+        askNumber += 1
+        
+        if askNumber == maxCountAsk {
+            title = "\(title) All the questions have been answered."
+            message = "\(message) To begin again, press 'Continue'"
+            
+            score = 0
+            askNumber = 0
+        }
+        
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+    }
 }
 
---
+++ README.md
@@ -0,0 +1,5 @@
+
+# 100 Days Of Swift
+## Day 21 
+### Project 2 - Guess the Flag UIkit
+
